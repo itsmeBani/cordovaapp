910acc4ea487c2ce83875264966e59233d4757d5
jiovani fabro <you@example.com>
2024-06-05 20:27:46 +0800

    first commit

---
+++ www/js/index.js
@@ -0,0 +1,8 @@
+// document.addEventListener('deviceready',function () {
+//     cordova.InAppBrowser.open('http://192.168.36.233/icatregistration/',
+//         '_blank',
+//         'location=no',
+//         'toolbar=no', 'zoom=no',
+//         'closebuttoncaption=Return',
+//         'EnableViewPortScale=no');
+// });
